8f30a9dcecece1d7949d0e9ae31097f58ebd6a49
shanyouyan <2436450335@qq.com>
2020-12-02 15:27:05 +0800

    添加商品搜索

--- pages/search/index.js
+++ pages/search/index.js
@@ -13,49 +13,48 @@ import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
   data: {
-    goods:[],
+    goods: [],
     // 取消 按钮 是否显示
-    isFocus:false,
+    isFocus: false,
     // 输入框的值
-    inpValue:""
+    inpValue: ""
   },
-  TimeId:-1,
+  TimeId: -1,
   // 输入框的值改变 就会触发的事件
-  handleInput(e){
+  handleInput(e) {
     // 1 获取输入框的值
-    const {value}=e.detail;
+    const { value } = e.detail;
     // 2 检测合法性
-    if(!value.trim()){
+    if (!value.trim()) {
       this.setData({
-        goods:[],
-        isFocus:false
+        goods: [],
+        isFocus: false
       })
       // 值不合法
       return;
     }
     // 3 准备发送请求获取数据
     this.setData({
-      isFocus:true
+      isFocus: true
     })
     clearTimeout(this.TimeId);
-    this.TimeId=setTimeout(() => {
+    this.TimeId = setTimeout(() => {
       this.qsearch(value);
     }, 1000);
   },
   // 发送请求获取搜索建议 数据
-  async qsearch(query){
-    const res=await request({url:"/goods/qsearch",data:{query}});
-    console.log(res);
+  async qsearch(query) {
+    const res = await request({ url: "/goods/qsearch", data: { query } });
     this.setData({
-      goods:res
+      goods: res.data.message
     })
   },
   // 点击 取消按钮
-  handleCancel(){
+  handleCancel() {
     this.setData({
-      inpValue:"",
-      isFocus:false,
-      goods:[]
+      inpValue: "",
+      isFocus: false,
+      goods: []
     })
   }
 })
